8f8af93ace26deab3d2d60db976d6d09b9584f22
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2025-05-26 22:55:22 +0200

    fix: add HF token

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -7,4 +7,6 @@ pip install tensorboard
 
 apt update && apt install -y ffmpeg
 
+echo $RUNPOD_SECRET_HUGGINGFACE_HUB_TOKEN > ~/.huggingface/token
+
 python train_cv.py hparams/train_ecapa_tdnn_mel_spec_cv.yaml --precision fp16 --grad_accumulation_factor 2 --debug
