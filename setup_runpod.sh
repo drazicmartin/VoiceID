67b9a5a58aa0b89092ca5ca7fee6718cd35335d2
drazicmartin <41931010+drazicmartin@users.noreply.github.com>
2025-05-27 22:02:47 +0200

    fix: env var name

--- setup_runpod.sh
+++ setup_runpod.sh
@@ -7,7 +7,7 @@ pip install tensorboard
 
 apt update && apt install -y ffmpeg
 
-huggingface-cli login --token $HUGGING_FACE_HUB_TOKEN
+huggingface-cli login --token $HUGGINGFACE_HUB_TOKEN
 
 
 python train_cv.py hparams/train_ecapa_tdnn_mel_spec_cv.yaml --precision fp16 --grad_accumulation_factor 2 --debug
